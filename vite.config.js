5287a7e913bd925c4f0ed8f4d017662ee877d340
Anish <anishdasmail@gmail.com>
2025-08-19 23:51:37 -0400

    Your update message

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/body-progress-tracker/',
+  base: '/tracker/',
   build: {
     outDir: 'dist',
     sourcemap: true
