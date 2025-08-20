aa88c0c4893303cea6ac64ae1d8c7904b6d6349b
Anish <anishdasmail@gmail.com>
2025-08-20 16:49:52 -0400

    Fix base path for tracker-copy repository and update title

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tracker/',
+  base: '/tracker-copy/',
   build: {
     outDir: 'dist',
     sourcemap: true
